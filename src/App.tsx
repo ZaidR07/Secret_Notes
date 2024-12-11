ea06d3b1b2cb21dc76719af667c8c6d012ffc003
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2024-12-11 22:56:19 +0530

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-
+import { BrowserRouter , Routes , Route } from "react-router-dom"
+import Login from "./Pages/Login"
 function App() {
  
 
   return (
     <>
-      <p>First</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element = {<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
