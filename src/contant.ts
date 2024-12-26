a52c0ca50267a79db58ec2ac396d0fa63953cd73
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2024-12-26 21:32:30 +0530

    uri chnaged

--- src/contant.ts
+++ src/contant.ts
@@ -1,7 +1,7 @@
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 
-// export const uri = "https://secret-notes-server.onrender.com/api/"
-export const uri = "http://localhost:8000/api/"
+export const uri = "https://secret-notes-server.onrender.com/api/"
+// export const uri = "http://localhost:8000/api/"
 
 export {faHeart as solidheart}
